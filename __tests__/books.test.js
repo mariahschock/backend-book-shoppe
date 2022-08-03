cc60922e308fcadfd4c3d079cc378724603b2c34
Mariah <mariahschock@gmail.com>
2022-08-02 17:22:26 -0700

    first test is passing for books. Models and controllers are all ago

---
+++ __tests__/books.test.js
@@ -0,0 +1,19 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should return a list of books with release dates', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(8);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+INSERT INTO books (
+  title,
+  released
+)
+
+VALUES
+  ('All the Light We Cannot See', 2014),
+  ('Cloud Cuckoo Land', 2021),
+  ('The Invisible Life of Addie LaRue', 2020),
+  ('The Art of Racing in the Rain', 2008),
+  ('The Secret Histroy', 1992),
+  ('The Goldfinch', 2013),
+  ('The Great Gatsby', 1925),
+  ('East of Eden', 1952);
+
